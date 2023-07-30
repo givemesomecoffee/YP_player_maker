b825189825e331cc937b380947a983749c9d4bfc
givemesomecoffee <givemesomecoffee@yandex.ru>
2023-07-30 23:31:13 +0300

    feature/sprint23/toolbar

--- app/src/main/java/givemesomecoffee/ru/playlistmaker/feature/main/NavBarsController.kt
+++ app/src/main/java/givemesomecoffee/ru/playlistmaker/feature/main/NavBarsController.kt
@@ -4,8 +4,13 @@ import androidx.fragment.app.Fragment
 
 interface NavBarsController {
     fun hideBottomNavigation(hide: Boolean)
+    fun setFullScreen(isFullScreen: Boolean)
 }
 
 fun Fragment.hideBottomNavigation(hide: Boolean = true){
     (requireActivity() as? NavBarsController)?.hideBottomNavigation(hide)
+}
+
+fun Fragment.setFullScreen(isFullScreen: Boolean = false){
+    (requireActivity() as? NavBarsController)?.setFullScreen(isFullScreen)
 }
